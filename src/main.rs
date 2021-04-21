d3a9adad0927cf6188868fcd91daa61f60f4ae35
Inversion <neoinversion@outlook.com>
2021-04-20 23:02:28 -0700

    Create very basic GUI

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,80 @@
-fn read_to_string(string: &mut String) {
-    let _bytes = std::io::stdin().read_line(string);
-}
+use iced::{Application, Settings, Command, Clipboard, executor, 
+    Element, Button, button, TextInput, text_input, Column, Text
+};
+use reqwest::{Client};
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = reqwest::Client::new();
-    let mut url = String::new();
+async fn main() -> iced::Result {
+    SendToWebhook::run(Settings {
+        window: iced::window::Settings {
+            size: (400, 200),
+            ..Default::default()
+        },
+        ..Default::default()
+    })
+}
 
-    println!("Enter webhook URL:");
-    read_to_string(&mut url);
+#[derive(Default)]
+struct SendToWebhook {
+    client: Client,
+    url: text_input::State,
+    url_value: String,
+    message: text_input::State,
+    message_value: String,
 
-    loop {
-        let mut input = String::new();
-        println!("Enter message or command ('STW_EXIT' or 'STW_CHANGE')");
-        read_to_string(&mut input);
+    send_button: button::State,
+}
 
-        if input.trim_end().eq("STW_EXIT") {
-            break;
-        }
-        else if input.trim_end().eq("STW_CHANGE") {
-            println!("Enter webhook URL:");
-            url.clear();
-            read_to_string(&mut url);
-        }
-        else {
-            let json = serde_json::json!({
-                "content": input,
-            });
-            let res = client.post(&url)
-                .json(&json)
-                .send()
-                .await?;
+#[derive(Debug, Clone)]
+pub enum UserInput {
+    SendPressed,
+    RequestSent,
+    URLChanged(String),
+    MessageChanged(String),
+}
+
+impl Application for SendToWebhook {
+    type Executor = executor::Default;
+    type Message = UserInput;
+    type Flags = ();
+
+    fn new(_flags: ()) -> (Self, Command<Self::Message>) {
+        (SendToWebhook::default(), Command::none())
+    }
+
+    fn title(&self) -> String {
+        String::from("SendToWebhook")
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Column::new()
+            .push(TextInput::new(&mut self.url, "Webhook URL", &mut self.url_value, UserInput::URLChanged))
+            .push(TextInput::new(&mut self.message, "Message", &mut self.message_value, UserInput::MessageChanged))
+            .push(Button::new(&mut self.send_button, Text::new("Send")).on_press(UserInput::SendPressed))
+            .into()
+    }
+
+    fn update(&mut self, input: UserInput, _clipboard: &mut Clipboard) -> Command<Self::Message> {
+        match input {
+            UserInput::SendPressed => {
+                let client = &self.client;
+                let json = serde_json::json!({
+                    "content": &self.message_value,
+                });
                 
-            // Should probably make this more clear to the user some time,
-            // since Discord returns 204 No Content on success (as it should!)
-            println!("Received Status: {}", res.status());
+                // Basically for testing
+                // println!("{} {} {}", self.url_value, self.message_value, json);
+
+                Command::perform(client.post(&self.url_value).json(&json).send(), |_response| {
+                    // In the future, we need to retrieve the status code from _response.
+                    // Currently (4/20/2021), my limited knowledge of Rust and the libraries involved
+                    // makes the resulting errors impossible to solve.
+                    UserInput::RequestSent
+                })
+            },
+            UserInput::RequestSent => {Command::none()},
+            UserInput::URLChanged(value) => {self.url_value = value; Command::none()},
+            UserInput::MessageChanged(value) => {self.message_value = value; Command::none()},
         }
     }
-    
-    Ok(())
 }
